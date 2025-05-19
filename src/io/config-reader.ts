02581db54e441ff6d22f6198cc5f1ec0920b8ac1
Leonid Medvedev <77212731+LeonPucin@users.noreply.github.com>
2025-05-19 16:59:45 +0300

    Update config reader

--- src/io/config-reader.ts
+++ src/io/config-reader.ts
@@ -19,6 +19,22 @@ export class ConfigReader {
      */
     public async readConfig<TConfig>(configPath: string, defaultConfig: TConfig): Promise<TConfig> {
         try {
+            return ConfigReader.readConfig(configPath, defaultConfig);
+        } catch (error) {
+            this.logger?.error(error instanceof Error ? error.message : String(error));
+            return defaultConfig;
+        }
+    }
+
+    /**
+     * Reads the configuration from the specified file and populates the given object with values from the file.
+     * @param {string} configPath - The path to the configuration file (e.g., './config.json').
+     * @param {TConfig} defaultConfig - The object that contains the default configuration values.
+     * @returns {Promise<TConfig>} A promise that resolves to the merged configuration object.
+     * @template TConfig - The type of the configuration object.
+     */
+    public static async readConfig<TConfig>(configPath: string, defaultConfig: TConfig): Promise<TConfig> {
+        try {
             const fullPath = path.resolve(configPath);
 
             const fileContent = await IOFile.read(fullPath);
@@ -27,8 +43,7 @@ export class ConfigReader {
 
             return Object.assign({}, defaultConfig, parsedConfig);
         } catch (error) {
-            this.logger?.error('Error reading or parsing the configuration file:', error);
-            return defaultConfig;
+            throw new Error('Error reading or parsing the configuration file: ' + error);
         }
     }
 }
