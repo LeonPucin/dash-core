93f51dffcf475a3bc093fe9b01c7ec5d6a19f50f
Leonid Medvedev <77212731+LeonPucin@users.noreply.github.com>
2025-05-20 20:43:22 +0300

    Update adaptive poller doc

--- src/threading/adaptive-poller.ts
+++ src/threading/adaptive-poller.ts
@@ -2,11 +2,22 @@ import { TimeSpan, ServiceLogger } from 'dash-core';
 
 
 type pollerOptions = {
+    /** Initial delay before starting the first poll cycle. */
     initialDelay: TimeSpan,
+
+    /** Maximum delay between polling cycles. */
     maxDelay: TimeSpan,
+
+    /** Factor by which the delay will be multiplied on each failure. */
     factor: number,
+
+    /** Time period after which the delay will reset to the initial delay if no errors occur. */
     resetPeriod: TimeSpan,
-    linearStep: TimeSpan
+
+    /** The step used for linear delay decrease. */
+    linearStep: TimeSpan,
+
+    /** Optional logger for logging events during polling. */
     logger?: ServiceLogger,
 }
 
@@ -43,6 +54,16 @@ export class AdaptivePoller {
         return this.polling;
     }
 
+    /**
+     * Creates an instance of the AdaptivePoller class.
+     * @param {pollerOptions} options - Configuration options for the polling mechanism.
+     * @param {TimeSpan} options.initialDelay - The initial delay before the first polling attempt.
+     * @param {TimeSpan} options.maxDelay - The maximum delay between polling attempts.
+     * @param {number} options.factor - The factor by which the delay will be multiplied after each failure.
+     * @param {TimeSpan} options.resetPeriod - The period after which the delay will reset to the initial value if no errors occur.
+     * @param {TimeSpan} options.linearStep - The step used to decrease the delay linearly on success.
+     * @param {ServiceLogger} [options.logger] - Optional logger to log events during polling.
+     */
     constructor(options: pollerOptions = defaultOptions) {
         this.options = options;
 
@@ -50,13 +71,12 @@ export class AdaptivePoller {
     }
 
     /**
-     * Starts the polling process.
-     * The polling will continue until explicitly stopped.
+     * Starts the polling process, which continues until explicitly stopped.
      * @param {() => Promise<void>} operation - The operation to perform during each poll cycle.
-     * @param {((error: Error) => void) | null} [onError=null] - A callback to handle errors during polling.
-     * @param {((error: Error) => void) | null} [onFail=null] - A callback to handle failure events (when the interval exceeds maxDelay).
+     * @param {(error: Error) => void} onError - Optional callback to handle errors during polling.
+     * @param {(error: Error) => void} onFail - Optional callback to handle failure events (when the interval exceeds maxDelay).
      */
-    public start(operation: () => Promise<void>, onError?: ((error: Error) => void), onFail?: ((error: Error) => void)) {
+    public start(operation: () => Promise<void>, onError?: (error: Error) => void, onFail?: (error: Error) => void) {
         if (this.polling) return;
 
         this.polling = true;
@@ -83,17 +103,17 @@ export class AdaptivePoller {
     }
 
     private async runPoll(): Promise<void> {
-        if (!this.polling || !this.operation)
-            return;
+        if (!this.polling || !this.operation) return;
 
         try {
             await this.operation();
 
             const now = Date.now();
 
+            // Reset the delay if no error occurred within the reset period
             if (!this.lastErrorTime || (now - this.lastErrorTime) >= this.options.resetPeriod.totalMilliseconds) {
                 const initialDelay = this.options.initialDelay.totalMilliseconds;
-                const linearDifference = this.currentDelay - this.options.linearStep.totalMilliseconds
+                const linearDifference = this.currentDelay - this.options.linearStep.totalMilliseconds;
 
                 this.currentDelay = Math.max(initialDelay, linearDifference);
             }
@@ -104,9 +124,11 @@ export class AdaptivePoller {
             const resultError = error instanceof Error ? error : new Error(JSON.stringify(error));
 
             if (this.currentDelay >= this.options.maxDelay.totalMilliseconds) {
+                // If delay exceeds maxDelay, trigger the onFail callback
                 this.currentDelay = this.options.maxDelay.totalMilliseconds;
                 this.onFail?.(resultError);
             } else {
+                // If an error occurred but delay is still within max, trigger the onError callback
                 this.onError?.(resultError);
             }
         } finally {
